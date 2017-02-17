c807ec0704a6802c5b616f4c4900a0c496229e08
Joshua Beard <beardj1@seattleu.edu>
2017-02-17 14:49:50 -0800

    flr_0.3.0
    Updated findLargestRects such that it returns values, created the
    /autochip/ package which contains findLargestRects.py and will
    eventually contain autochip.py, also includes /test/ for testing

---
+++ autochip/test/driver_findLargestRects.py
@@ -0,0 +1,15 @@
+# driver_findLargestRects.py
+'''
+This is a driver for findLargestRects.py
+
+Author: Joshua Beard
+C: 2/10/17
+E: 2/10/17
+
+'''
+import numpy as np
+import math
+import scipi.io as sio
+
+
+template = sio.loadmat('template0.mat')	# Load up the template generated in ML
