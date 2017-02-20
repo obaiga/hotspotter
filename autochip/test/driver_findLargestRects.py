ae99f543beb5192a323812d89000e9880bdf14ec
JoshuaBeard <beardj1@seattleu.edu>
2017-02-20 13:45:13 -0800

    driver_flr

--- autochip/test/driver_findLargestRects.py
+++ autochip/test/driver_findLargestRects.py
@@ -4,12 +4,18 @@ This is a driver for findLargestRects.py
 
 Author: Joshua Beard
 C: 2/10/17
-E: 2/10/17
+E: 2/20/17
 
 '''
 import numpy as np
 import math
 import scipi.io as sio
 
+''' Parameters '''
+templateName = 'template0.mat'
 
-template = sio.loadmat('template0.mat')	# Load up the template generated in ML
+''' Initialization '''
+
+
+matContents = sio.loadmat('template0.mat')	# Load up the template generated in ML
+template = matContents['template'] 			# get the actual template
