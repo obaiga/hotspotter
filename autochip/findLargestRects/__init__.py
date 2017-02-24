3fc0c31443af92abdc33a534abd47eb02a528d59
JoshuaBeard <beardj1@seattleu.edu>
2017-02-24 13:16:16 -0800

    changed variable names for compatibility with pdb

--- autochip/findLargestRects/__init__.py
+++
@@ -1,4 +0,0 @@
-''' Init file for flr
-Joshua Beard
-2/20/17
-'''
--- autochip/test/driver_findLargestRects.py
+++ autochip/test/driver_findLargestRects.py
@@ -4,27 +4,32 @@ This is a driver for findLargestRects.py
 
 Author: Joshua Beard
 C: 2/10/17
-E: 2/23/17
+E: 2/24/17
 	2/23/17 
 		Got importing working alright.
 		Initial tests result in bounds = (670, 223, 349, 349), which is curious specifically the width and height of the largest rectangle are both supposedly 349, which seems unlikely. I need to go back to MATLAB to verify using this template.
 		It seems the maxW and maxH matrices do not get updated with new widths and heights, as 349 is the largest square side returned from findLargestSquares
 
 '''
-import numpy as np
-import math
+#import numpy as np
+#import math
 import scipy.io as sio
 import sys
+
+''' Parameters '''
 # BEWARE the hardcoded path
-sys.path.append('/home/joshuabeard/Documents/hotspotter')
+hsPath = '/home/joshuabeard/Documents/hotspotter'
+testPath = '/home/joshuabeard/Documents/hotspotter/autochip/test'
+templateName = 'template0.mat'
+
+''' More imports after defining parameters '''
+sys.path.append(hsPath)
+sys.path.append(testPath)
 import autochip.findLargestRects as flr
 
 
-''' Parameters '''
-templateName = 'template0.mat'
-
 ''' Initialization '''
-matContents = sio.loadmat(templateName)	# Load up the template generated in ML
+matContents = sio.loadmat(testPath + '/' + templateName)	# Load up the template generated in ML
 template = matContents['template'] 		# get the actual template
 
 largestRect = flr.findLargestRects(template)
