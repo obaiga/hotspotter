a172109685eb095c983a575a24b563e453c2f0f5
Joshua Beard <beardj1@seattleu.edu>
2017-03-29 14:11:14 -0700

    change stopCrit
    change autochip stopping criterion to 3 chips

--- hotspotter/HotSpotterAPI.py
+++ hotspotter/HotSpotterAPI.py
@@ -622,7 +622,7 @@ class HotSpotter(DynStruct):
     @profile # IhavenoideawhatImdoing
     #@helpers.indent_decor('[hs.autochip]') #mine doesn't recognize helpers
     def autochip(hs, directoryToTemplates, exclFac = 1, stopCrit = .9, skip = 8, crit = [0,0,1], minSize = [1,1]):
-        chipDict = ac.doAutochipping(directoryToTemplates, exclFac = 1, stopCrit = .9, skip = 8, crit = [0,0,1], minSize = [1,1])
+        chipDict = ac.doAutochipping(directoryToTemplates, exclFac = 1, stopCrit = 3, skip = 8, crit = [0,0,1], minSize = [1,1])
         print(chipDict)
         chipNum = 0;
         for image in chipDict:
