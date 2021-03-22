92ff7c556678970af7a83a451012581536182cd1
obaiga <61898614+obaiga@users.noreply.github.com>
2021-03-22 14:10:35 -0600

    update

--- _scripts/run_profile.sh
+++
@@ -1,4 +0,0 @@
-#kernprof.py -l dev.py --db MOTHERS -t vsmany_best --nocache-query
-#python -m line_profiler dev.py.lprof
-kernprof.py -l dev.py --db NAUTS -t dists
-python -m line_profiler dev.py.lprof
